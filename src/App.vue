1789a3bc843f6f5dc871544ca3866cc568fc24f4
maria-arce <maria.arce@wearelean.com>
2021-10-27 11:22:33 +0200

    Add another sample page

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="nav">
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">About</router-link> |
+     <router-link to="/contact">Another sample page</router-link>
   </div>
   <router-view/>
 </template>
